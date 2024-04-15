0dd200ef83a9fe741a760a75d91bfc98deed3fec
UDEME JALEKUN <ubassey@myvoluntier.com>
2024-04-15 22:18:09 +0100

    weektwo

---
+++ cypress.config.js
@@ -0,0 +1,86 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://staging.trymima.com/',
+    viewportHeight: 960,
+    viewportWidth: 1538,
+    defaultCommandTimeout: 10000,
+    chromeWebSecurity: true,
+    watchForFileChanges: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    browsers: [
+      {
+        name: 'chrome',
+
+        family: 'chromium',
+
+        channel: 'stable',
+
+        displayName: 'Chrome',
+
+        version: '123.0.6312.107',
+
+        path: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+
+        minSupportedVersion: 64,
+
+        majorVersion: '123',
+
+      },
+      {
+        name: 'firefox',
+
+        family: 'firefox',
+
+        channel: 'stable',
+
+        displayName: 'Firefox',
+
+        version: '124.0.2',
+
+        path: '/Applications/Firefox.app/Contents/MacOS/firefox',
+
+        minSupportedVersion: 86,
+
+        majorVersion: '124',
+
+      },
+      {
+        name: 'electron',
+
+        channel: 'stable',
+
+        family: 'chromium',
+
+        displayName: 'Electron',
+
+        version: '118.0.5993.159',
+
+        path: '',
+
+        majorVersion: 118,
+
+      },
+      {
+        name: 'Brave',
+
+        channel: 'stable',
+
+        family: 'chromium',
+
+        displayName: 'Brave',
+
+        version: '123.0.6312.105',
+
+        path: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser', 
+
+        majorVersion:'',
+
+      },
+    ],
+    experimentalWebKitSupport: true
+  },
+});
---
+++ cypress/e2e/signup/signup.spec.cy.js
@@ -0,0 +1,136 @@
+const { faker } = require("@faker-js/faker")
+let data
+let basic
+let other
+describe('Sign Up Journey', () => {
+  before(()=>{
+    cy.fixture('selectors').then(sel=>{
+      data = sel
+      basic = data.basicDetailsPage
+      other = data.otherDetailsPage
+    })
+  })
+  beforeEach(()=>{
+    cy.visit('/')
+    cy.contains('Sign up').should('be.visible').click()
+    cy.get(basic.fullnameField).should('be.visible').fill('Osman Shire')
+    cy.get(basic.businessnameField).fill('Eniola')
+    cy.get(basic.businessEmailField).fill(faker.internet.email())
+    cy.get(basic.businessPhoneNumberField).fill(faker.phone.number('+23480########'))
+    cy.get(basic.businessRegNumberField).fill('RC-0987')
+    cy.contains('Next').click() 
+  })
+  it('A successful sign up with all fields filled', () => {
+    cy.get(other.WebField).should('be.visible').fill('https://www.osmanshire.org')
+    cy.get(other.igField).fill('@osmanshire')
+    cy.get(other.xField).fill('@osmanshire')
+    cy.get(other.heardAboutUs).click()
+    cy.contains('Google Search').should('be.visible').click()
+    cy.get(other.passwordField).fill('Test1234@')
+    cy.contains('Sign Up').click()
+  })
+
+  it('A successful sign up with only required fields filled', () => {
+    cy.contains('Sign Up').should('be.visible').click()
+    cy.get(data.fullnameField).should('be.visible').fill('Osman Shire')
+    cy.get(data.businessnameField).fill('Eniola')
+    //cy.get(data.businessEmailField).fill(faker.internet.email())
+    cy.mailslurp().then(mailslurp=> mailslurp.createInbox().then(inbox=>{
+      const inboxId = inbox.id
+      const emailAddress = inbox.emailAddress
+      cy.get(data.businessEmailField).fill(emailAddress())
+    }))
+    cy.get(data.businessPhoneNumberField).fill(faker.phone.number('+23480########'))
+    cy.get(data.businessRegNumberField).fill('RC-0987')
+    cy.contains('Next').click()
+    cy.get(data.marketingSourceDropdown).click()
+    cy.contains('Google Search').should('be.visible').click()
+    cy.get(data.passwordField).fill('Test1234@')
+    cy.contains('Sign Up').click()
+  })
+
+  it('A successful sign up with Google Search Option', () => {
+      cy.contains('Sign up').should('be.visible').click()
+      cy.get(data.fullnameField).should('be.visible').fill('Osman Shire')
+      cy.get(data.businessnameField).fill('Eniola')
+      cy.get(data.businessEmailField).fill(faker.internet.email({provider: 'yopmail.com'}))
+      cy.get(data.businessPhoneNumberField).fill(faker.phone.number('+23480########'))
+      cy.get(data.businessRegNumberField).fill('RC-0987')
+      cy.contains('Next').click()
+      cy.get(data.customerWebsiteField).should('be.visible').fill('https://www.osmanshire.org')
+      cy.get(data.igField).fill('@osmanshire')
+      cy.get(data.xField).fill('@osmanshire')
+      cy.get(data.marketingSourceDropdown).click()
+      cy.contains('Google Search').should('be.visible').click()
+      cy.get(data.passwordField).fill('Test1234@')
+      cy.contains('Sign Up').click()
+  })
+
+  it('A successful sign up with Instagram Option', () => {
+    cy.contains('Sign up').should('be.visible').click()
+    cy.get(data.fullnameField).should('be.visible').fill('Osman Shire')
+    cy.get(data.businessnameField).fill('Eniola')
+    cy.get(data.businessEmailField).fill(faker.internet.email())
+    cy.get(data.businessPhoneNumberField).fill(faker.phone.number('+23480########'))
+    cy.get(data.businessRegNumberField).fill('RC-0987')
+    cy.contains('Next').click()
+    cy.get(data.customerWebsiteField).should('be.visible').fill('https://www.osmanshire.org')
+    cy.get(data.igField).fill('@osmanshire')
+    cy.get(data.xField).fill('@osmanshire')
+    cy.get(data.marketingSourceDropdown).click()
+    cy.contains('Instagram').should('be.visible').click()
+    cy.get(data.passwordField).fill('Test1234@')
+    cy.contains('Sign Up').click()
+  })
+
+  it('A successful sign up with Facebook Option', () => {
+    cy.contains('Sign up').should('be.visible').click()
+    cy.get(data.fullnameField).should('be.visible').fill('Osman Shire')
+    cy.get(data.businessnameField).fill('Eniola')
+    cy.get(data.businessEmailField).fill(faker.internet.email())
+    cy.get(data.businessPhoneNumberField).fill(faker.phone.number('+23480########'))
+    cy.get(data.businessRegNumberField).fill('RC-0987')
+    cy.contains('Next').click()
+    cy.get(data.customerWebsiteField).should('be.visible').fill('https://www.osmanshire.org')
+    cy.get(data.igField).fill('@osmanshire')
+    cy.get(data.xField).fill('@osmanshire')
+    cy.get(data.marketingSourceDropdown).click()
+    cy.contains('Facebook').should('be.visible').click()
+    cy.get(data.passwordField).fill('Test1234@')
+    cy.contains('Sign Up').click()
+  })
+
+  it('A successful sign up with Twitter Option', () => {
+    cy.contains('Sign up').should('be.visible').click()
+    cy.get(data.fullnameField).should('be.visible').fill('Osman Shire')
+    cy.get(data.businessnameField).fill('Eniola')
+    cy.get(data.businessEmailField).fill(faker.internet.email())
+    cy.get(data.businessPhoneNumberField).fill(faker.phone.number('+23480########'))
+    cy.get(data.businessRegNumberField).fill('RC-0987')
+    cy.contains('Next').click()
+    cy.get(data.customerWebsiteField).should('be.visible').fill('https://www.osmanshire.org')
+    cy.get(data.igField).fill('@osmanshire')
+    cy.get(data.xField).fill('@osmanshire')
+    cy.get(data.marketingSourceDropdown).click()
+    cy.contains('Twitter').should('be.visible').click()
+    cy.get(data.passwordField).fill('Test1234@')
+    cy.contains('Sign Up').click()
+  })
+
+  it('A successful sign up with Others Option', () => {
+    cy.contains('Sign up').should('be.visible').click()
+    cy.get(data.fullnameField).should('be.visible').fill('Osman Shire')
+    cy.get(data.businessnameField).fill('Eniola')
+    cy.get(data.businessEmailField).fill(faker.internet.email())
+    cy.get(data.businessPhoneNumberField).fill(faker.phone.number('+23480########'))
+    cy.get(data.businessRegNumberField).fill('RC-0987')
+    cy.contains('Next').click()
+    cy.get(data.customerWebsiteField).should('be.visible').fill('https://www.osmanshire.org')
+    cy.get(data.igField).fill('@osmanshire')
+    cy.get(data.xField).fill('@osmanshire')
+    cy.get(data.marketingSourceDropdown).click()
+    cy.contains('Others').should('be.visible').click()
+    cy.get(data.passwordField).fill('Test1234@')
+    cy.contains('Sign Up').click()
+  })
+})
